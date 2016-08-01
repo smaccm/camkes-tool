3fdc578394eb5045466edbee62e1695a7964ec4e
Gamlath, Kalana (Data61, Kensington UNSW) <Kalana.Gamlath@data61.csiro.au>
2016-08-01 16:50:33 +1000

    Added copyright headers to files that were missing them

--- debug/templates/gdb.h
+++ debug/templates/gdb.h
@@ -1,3 +1,13 @@
+/*#
+ *# Copyright 2014, NICTA
+ *#
+ *# This software may be distributed and modified according to the terms of
+ *# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ *# See "LICENSE_BSD2.txt" for details.
+ *#
+ *# @TAG(NICTA_BSD)
+ #*/
+
 #define NO_BREAKPOINT -1
 #define USER_BREAKPOINT 0
 #define BREAKPOINT_INSTRUCTION 0xCC
